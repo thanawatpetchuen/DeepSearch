bd781664e5c08bcab595917265a944274d79e106
thanawatpetchuen <31251288+thanawatpetchuen@users.noreply.github.com>
2018-05-08 23:45:45 +0700

    re

--- recursivetest.py
+++ recursivetest.py
@@ -8,6 +8,8 @@ import sys
 import re
 import time
 from operator import itemgetter
+import shutil
+import os
 
 
 class wordCount:
@@ -81,100 +83,105 @@ class htmlparser(wordCount):
         self.read_to_temp_html()
 
 
-def deepsearch(link, temp):
-    if len(temp) == 0:
-        return
-    else:
-        for link2 in temp:
-            print(link2)
-            try:
-                html_temp2 = htmlparser(link2, '{}.txt'.format(
-                    link.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
-                html_temp2.setup()
-                bs_html2 = html_temp2.beauiful_soup()
-                tree.create_node(link2, link2, parent=link, data={'related_link': bs_html2, 'count': html_temp2.Count2()})
-                if len(bs_html2) > 0:
-                    return deepsearch(link2, bs_html2)
-            except:
-                print(sys.exc_info())
-    # pass
 
 def update_percent(temp, p):
     return (p/int(len(temp)))*100
 
+class deep_search:
+    def __init__(self):
+        url = 'https://en.wikipedia.org/wiki/Computer'
+        html = htmlparser(url, '{}.txt'.format(url.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
+        html.setup()
+        self.html_dict = dict()
+        temp = html.beauiful_soup()
+        self.tree = Tree()
+        self.level = 0
+        # print(len(temp))
+        # self.temp = temp[0:100]
+        self.temp = temp
+        self.percent = 0
+        print("Percent: {}%".format(update_percent(temp, self.percent)))
+        self.tree.create_node("Root", "root", data={'related_link': temp, 'count': html.Count2()})
+        self.unsearch = []
+
+    def deepsearch(self, link, temp):
+        if len(temp) == 0:
+            return
+        if self.level > 2:
+            print("Over!")
+            print("Level: ", self.level)
+            self.level = 0
+            self.unsearch.append(self.tree.get_node(link))
+            return
+        else:
+            print("Level: ", self.level)
+            for link2 in temp:
+                print(link2)
+                try:
+                    html_temp2 = htmlparser(link2, '{}.txt'.format(
+                        link.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
+                    html_temp2.setup()
+                    bs_html2 = html_temp2.beauiful_soup()
+                    self.tree.create_node(link2, link2, parent=link, data={'related_link': bs_html2, 'count': html_temp2.Count2()})
+                    if len(bs_html2) > 0:
+                        self.level += 1
+                        return self.deepsearch(link2, bs_html2)
+                except:
+                    print(sys.exc_info())
+        # pass
+
+    def search(self):
+        for link in self.temp:
+            try:
+                html_temp = htmlparser(link, '{}.txt'.format(link.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
+                html_temp.setup()
+                bs_html = html_temp.beauiful_soup()
+                self.html_dict[link] = [bs_html, html_temp.Count2()]
+                self.tree.create_node(link, link, parent='root', data={'related_link': bs_html, 'count': html_temp.Count2()})
+                if len(bs_html) > 0:
+                    self.level += 1
+                    print("Level: ", self.level)
+                    self.deepsearch(link, bs_html)
+                # print(bs_html)
+                self.percent += 1
+                print("Percent: {}%".format(update_percent(self.temp, self.percent)))
+                self.tree.show()
+            except:
+                self.percent += 1
+                print("Percent: {}%".format(update_percent(self.temp, self.percent)))
+                print(sys.exc_info())
+
+        self.finalize()
+
+    def finalize(self):
+
+        f = open('final.txt', 'w+', encoding='utf-8')
+        f.write(str(self.html_dict))
+        f.close()
+        # print(html_dict)
+        print(self.tree.to_json(with_data=True))
+        self.tree.show()
+
+        start = time.time()
+        temp_tree = [{'link': self.tree[node].tag, 'count': self.tree[node].data['count']['computer']} for node in self.tree.expand_tree(mode=self.tree.ZIGZAG)
+                     if (self.tree[node].data is not None)]
+        stop = time.time()
+        print(self.tree['root'].data)
+        print(temp_tree)
+        newlist = sorted(temp_tree, key=itemgetter('count'), reverse=True)
+        print("Total time: {}s".format(stop-start))
+        print(newlist)
+        with open('result/tree.json', 'w+') as fs:
+            fs.write(self.tree.to_json(with_data=True))
+        self.tree.save2file('result/tree.txt')
+        print("SUCCESS, Percent: {}%".format(update_percent(self.temp, self.percent)))
+        print("=======Unsearch=======")
+        print(self.unsearch)
+        print("======================")
+        shutil.rmtree('temp')
+        os.mkdir('temp')
+
 
 if __name__ == '__main__':
-    url = 'https://en.wikipedia.org/wiki/Computer'
-    html = htmlparser(url, '{}.txt'.format(url.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
-    html.setup()
-    html_dict = dict()
-    temp = html.beauiful_soup()
-    tree = Tree()
-    # print(len(temp))
-    temp = temp[0:50]
-    percent = 0
-    print("Percent: {}%".format(update_percent(temp, percent)))
-    tree.create_node("Root", "root", data={'related_link': temp, 'count': html.Count2()})
-    # print(html.Count2())
-    # for link in temp:
-    #     deepsearch('root', temp)
-    for link in temp:
-        try:
-            html_temp = htmlparser(link, '{}.txt'.format(link.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
-            html_temp.setup()
-            bs_html = html_temp.beauiful_soup()
-            html_dict[link] = [bs_html, html_temp.Count2()]
-            tree.create_node(link, link, parent='root', data={'related_link': bs_html, 'count': html_temp.Count2()})
-            if len(bs_html) > 0:
-                deepsearch(link, bs_html)
-            # print(bs_html)
-            percent += 1
-            print("Percent: {}%".format(update_percent(temp, percent)))
-            tree.show()
-        except:
-            percent += 1
-            print("Percent: {}%".format(update_percent(temp, percent)))
-            print(sys.exc_info())
-    # for link in temp:
-    #     try:
-    #         html_temp = htmlparser(link, '{}.txt'.format(link.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
-    #         html_temp.setup()
-    #         bs_html = html_temp.beauiful_soup()
-    #         html_dict[link] = [bs_html, html_temp.Count2()]
-    #         tree.create_node(link, link, parent='root', data={'related_link': bs_html, 'count': html_temp.Count2()})
-    #         if len(bs_html) > 0:
-    #             for link2 in bs_html:
-    #                 html_temp2 = htmlparser(link2, '{}.txt'.format(link.split('//')[-1].replace('/', '').replace('.', '').replace('=', '').replace('?', '')))
-    #                 html_temp2.setup()
-    #                 bs_html2 = html_temp2.beauiful_soup()
-    #                 tree.create_node(link2, link2, parent=link, data={'related_link': bs_html2, 'count': html_temp2.Count2()})
-    #         # print(bs_html)
-    #         percent += 1
-    #         print("Percent: {}%".format(update_percent(temp, percent)))
-    #         tree.show()
-    #     except:
-    #         percent += 1
-    #         print("Percent: {}%".format(update_percent(temp, percent)))
-    #         print(sys.exc_info())
-
-    f = open('final.txt', 'w+', encoding='utf-8')
-    f.write(str(html_dict))
-    f.close()
-    # print(html_dict)
-    print(tree.to_json(with_data=True))
-    tree.show()
-
-    start = time.time()
-    temp_tree = [{'link': tree[node].tag, 'count': tree[node].data['count']['computer']} for node in tree.expand_tree(mode=tree.ZIGZAG)
-                 if (tree[node].data is not None)]
-    stop = time.time()
-    print(tree['root'].data)
-    print(temp_tree)
-    newlist = sorted(temp_tree, key=itemgetter('count'), reverse=True)
-    print("Total time: {}s".format(stop-start))
-    print(newlist)
-    with open('result/tree.json', 'w+') as fs:
-        fs.write(tree.to_json(with_data=True))
-    tree.save2file('result/tree.txt')
-    print("SUCCESS, Percent: {}%".format(update_percent(temp, percent)))
-
+    dp = deep_search()
+    dp.search()
